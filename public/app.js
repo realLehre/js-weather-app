19d322e1d7023bf3cbbb9de6282336df1ca84045
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-24 08:09:29 +0100

    updates

--- public/app.js
+++ public/app.js
@@ -1,1 +1,10 @@
-alert(1)
+async function getWeather() {
+    const response = await fetch('http://api.weatherapi.com/v1/forecast.json?key=bb7034c07f3a4831a76103340222303&q=Lagos');
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+getWeather()
+    .then(data => data)
