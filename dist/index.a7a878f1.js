c7238d1676e6510d12a10abadc671ef1f88fbf12
David Egbuwalo <lereegbuwalo@gmail.com>
2022-03-25 09:21:34 +0100

    added weather object

--- dist/index.a7a878f1.js
+++ dist/index.a7a878f1.js
@@ -1,2 +1,8 @@
+class Weather {
+    constructor(city){
+        this.city = city;
+        this.key = 'bb7034c07f3a4831a76103340222303';
+    }
+}
 
 //# sourceMappingURL=index.a7a878f1.js.map
---
+++ public/ui.js
@@ -0,0 +1,6 @@
+class Weather{
+    constructor(city){
+        this.city = city;
+        this.key = 'bb7034c07f3a4831a76103340222303';
+    }
+}
